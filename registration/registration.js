decf5285141189e84253386d86f81384ecfdc210
Fil.ua <yuriifilimonov1@gmail.com>
2020-03-22 02:32:37 +0200

    new request

--- registration/registration.js
+++ registration/registration.js
@@ -9,38 +9,38 @@ document.querySelector(`#form_id`).addEventListener(`submit`, function (e) {
 function validate() {
 
     let names = document.querySelectorAll('#form_id input');
-    const objForm = {};
+    const values = {};
 
     for (let k = 0; k < names.length - 1; k++) {
-        objForm[names[k].id] = names[k].value;
+        values[names[k].id] = names[k].value;
     }
 
-    for (let x in objForm) {
+    for (let x in values) {
         switch (x) {
             case `SignupName`:
             case `SignupSurname`:
-                if (!/^([а-яА-ЯA-Za-z]{2,20})+$/.test(objForm[x])) {
+                if (!/^([а-яА-ЯA-Za-z]{2,20})+$/.test(values[x])) {
                     return warning(`Некорректный ввод!`);
                 }
                 break;
             case `SignupBirthday`:
-                let year = new Date().getFullYear() - new Date(objForm[x].replace(/\./g, `:`)).getFullYear();
+                let year = new Date().getFullYear() - new Date(values[x].replace(/\./g, `:`)).getFullYear();
                 if (!(10 < year && year < 80)) {
                     return warning(`не младше 10 и не старше 80 лет`);
                 }
                 break;
             case `SignupEmail`:
-                if (!/[0-9a-zа-я_A-ZА-Я]+@[0-9a-zа-я_A-ZА-Я^.]+\.[a-zа-яА-ЯA-Z]{2,4}/i.test(objForm[x])) {
+                if (!/[0-9a-zа-я_A-ZА-Я]+@[0-9a-zа-я_A-ZА-Я^.]+\.[a-zа-яА-ЯA-Z]{2,4}/i.test(values[x])) {
                     return warning(`Ошибка ввода Email-адреса!`);
                 }
                 break;
             case `SignupPassword`:
-                if (!/^([а-яА-ЯA-Za-z0-9]{6,})+$/.test(objForm[x])) {
+                if (!/^([а-яА-ЯA-Za-z0-9]{6,})+$/.test(values[x])) {
                     return warning(`Invalid Password!`);
                 }
                 break;
             case `SignupConfirmPassword`:
-                if (objForm.SignupPassword !== objForm[x]) {
+                if (values.SignupPassword !== values[x]) {
                     return warning(`Пароли не совпадают`);
                 }
                 break;
@@ -49,7 +49,7 @@ function validate() {
     function warning(x) {
         return !(document.getElementsByClassName(`warning`)[0].textContent = x);
     }
-    localStorage.setItem(objForm.SignupEmail, objForm.SignupPassword);
+    localStorage.setItem(`authData`, JSON.stringify([values.SignupName, values.SignupEmail, values.SignupPassword]));
     warning(``);
     return true;
 }
--- registration/start.js
+++ registration/start.js
@@ -1,10 +1,8 @@
-document.querySelector(`#form_id`).addEventListener(`submit`, function (e) {
+document.querySelector(`#form_id`).addEventListener(`submit`,  (e)=> {
     e.preventDefault();
     if(signIn()) {
         let str = ["spider","css-form","spiderNetBotR","displayed"];
-        for (let i in str) {
-            document.querySelector(`.${str[i]}`).classList.add("hide");
-        }
+        str.map(classes =>  document.querySelector(`.${classes}`).classList.add("hide"));
         document.querySelector('body').classList.add("strawberry");
     }
     return false;
@@ -12,36 +10,37 @@ document.querySelector(`#form_id`).addEventListener(`submit`, function (e) {
 function signIn() {
 
     let names = document.querySelectorAll('#form_id input');
-    const objForm = [];
+    const values = {};
 
     for (let k = 0; k < names.length - 1; k++) {
-        objForm[k] = names[k].value;
+        values[names[k].id] = names[k].value;
     }
+    console.log(values);
 
-    for (let x in objForm) {
+    for (let x in values) {
 
-        switch (+x) {
-            case 0:
-                if (!/[0-9a-zа-я_A-ZА-Я]+@[0-9a-zа-я_A-ZА-Я^.]+\.[a-zа-яА-ЯA-Z]{2,4}/i.test(objForm[x])) {
+        switch (x) {
+            case `SigninEmail`:
+                if (!/[0-9a-zа-я_A-ZА-Я]+@[0-9a-zа-я_A-ZА-Я^.]+\.[a-zа-яА-ЯA-Z]{2,4}/i.test(values[x])) {
                    return  warning("Please type a correct email!");
                 }
                 break;
-            case 1:
-                if (!/^([а-яА-ЯA-Za-z0-9]{6,})+$/.test(objForm[x])) {
+            case `SigninPassword`:
+                if (!/^([а-яА-ЯA-Za-z0-9]{6,})+$/.test(values[x])) {
                    return  warning("password must include more then 6 simbols!");
                 }
                 break;
         }
     }
 
-let data = localStorage.getItem(objForm[0]);
-return data ?
-    data == objForm[1] ? true : warning(`Wrong password`)
-    : warning("email doen`t exist in base");
+    let authData = JSON.parse(localStorage.getItem(`authData`));
+    return values.SigninEmail == authData[1] ?
+            authData[2] == values.SigninPassword ? true : warning(`Wrong password`)
+            : warning("email doen`t exist in base");
 
-    function warning(x) {
-        return !(document.getElementsByClassName(`warning`)[0].textContent = x);
-    };
+        function warning(x) {
+            return !(document.getElementsByClassName(`warning`)[0].textContent = x);
+        };
     warning(``);
 }
 
